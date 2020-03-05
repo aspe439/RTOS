f64f60781de34bbd4e73f8851aa8d91b839dbe25
Flyingcat965 <57508232+Flyingcat965@users.noreply.github.com>
2020-03-05 14:49:29 -0600

    merged

--- RTOS_Lab3_RTOSpriority/timer2a.d
+++ RTOS_Lab3_RTOSpriority/timer2a.d
@@ -1,9 +1,3 @@
-<<<<<<< HEAD
 .\timer2a.o: ..\inc\Timer2A.c
 .\timer2a.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
 .\timer2a.o: ..\inc\../inc/tm4c123gh6pm.h
-=======
-.\timer2a.o: ..\inc\Timer2A.c
-.\timer2a.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
-.\timer2a.o: ..\inc\../inc/tm4c123gh6pm.h
->>>>>>> 91491d35bebd4eebaf701e4c39376f8dec5ccffe
